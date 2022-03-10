54bb2ba9e372611ef81a6f6152bf23eaa216c896
baeninoise <amandalaleye@gmail.com>
2022-03-10 07:42:22 -0800

    rtr

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>                                                
+#include <conio.h>                                                
+/***
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ **/
+int main(void)                                                    {
+	clrscr();
+	printf("Size of a char:%2d byte(s) \n",sizeof(signed char));
+	printf("Size of an int: %2d byte(s) \n", sizeof(int));      
+	printf("Size of a long int: %2d byte(s) \n", sizeof(int *));
+	printf("Size of a long long int: %2d byte(s) \n", sizeof(lon
+	printf("Size of a float: %2d byte(s) \n",sizeof(float));    
+	getch()                                                   
+} 

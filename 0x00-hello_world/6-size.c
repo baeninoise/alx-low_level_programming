976cbf55e2d68f951a770dfccc9f023d32d1f448
baeninoise <amandalaleye@gmail.com>
2022-03-10 09:26:55 -0800

    rt

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,15 +7,15 @@
 int main(voip)
 {
 	char charType;
-	int integertype;
-	long int Type;
+	int integerType;
+	long int longintType;
 	long long int doubleType;
 	float floatType;
 
-	printf("Size of a char: %i byte(s)\n", sizeof(charType));
-	printf("Size of an int: %i byte(s)\n", sizeof(integerType));
-	printf("Size of a long int: %i byte(s)\n", sizeof(Type));
-	printf("Size of a long long int: %i byte(s)\n", sizeof(doubleType));
-	printf("Size of a float: %i byte(s)\n", sizeof(floatType));
+	printf("Size of a char: %d byte(s)\n", sizeof(charType));
+	printf("Size of an int: %d byte(s)\n", sizeof(integerType));
+	printf("Size of a long int: %d byte(s)\n", sizeof(longintType));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(doubleType));
+	printf("Size of a float: %d byte(s)\n", sizeof(floatType));
 	return (0);
 }

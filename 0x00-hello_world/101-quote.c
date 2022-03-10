9fc660df8ddef2fd2e7dc1f311d6a7023f3a429b
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-11 00:56:09 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,6 @@
  */
 int main(void)
 {
-  printf(2,
-		  "and that piece of art is useful\" - Dora Korpar, 2015-10-19",25); 
+  printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19"); 
   return (1);
 }}

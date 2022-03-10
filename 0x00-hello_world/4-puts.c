7ab6f35c1a1190dcfd33ec71aab36ae4782463c0
baeninoise <amandalaleye@gmail.com>
2022-03-10 05:38:16 -0800

    rt

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
-#include <stdio.h>                                                                                                                      
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
 
-#include <string.h>                                                                                                                     
-
-/**                                                                                                           *     */                                                                                                                                                      
-int main()                                                                                                                              
-
-	                                                                                                                                        
-
-{                                                                                                                                       
-
-	 char str[]="\"Programming is like building a multilingual puzzle";                                                                           puts(str);                                                                                                     
-  return (0);                                                                                                                           
-		                                                                                                                       
-} 
+	puts(str);
+	return (0);
+}

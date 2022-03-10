aec540811695db4ce7afd2b8d51825d27fbd292a
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-10 17:45:04 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
-/***
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
- **/
-int main(void)                                                    {
-	clrscr();
-	printf("Size of a char:%2d byte(s) \n",sizeof(signed char));
-	printf("Size of an int: %2d byte(s) \n", sizeof(int));      
-	printf("Size of a long int: %2d byte(s) \n", sizeof(long int));
-	printf("Size of a long long int: %2d byte(s) \n", sizeof(long long int));
-	printf("Size of a float: %2d byte(s) \n",sizeof(float));    
-	getch();
-} 
+ */
+int main(voip)
+{   
+	char charType;
+	int integertype;
+	long int Type;
+	long long doubleType;
+	float floatType;
+    
+	printf("Size of a char: %ld byte(s)\n",sizeof(charType));
+	printf("Size of an int: %ld byte(s)\n",sizeof(integerType));
+	printf("Size of a long long: %ld byte(s)\n",sizeof(doubleType));
+	printf("Size of a long long: %ld byte(s)\n",sizeof(longintType));
+	printf("Size of a float: %ld byte(s)\n",sizeof(floatType));
+	return 0;
+}

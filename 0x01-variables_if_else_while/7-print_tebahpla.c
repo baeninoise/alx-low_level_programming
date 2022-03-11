468df8e4bec2627b29527197a0e1d12bc0c62d49
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-11 22:03:31 +0100

    Create 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	int x = 'z';
+
+	while (x <= 'a')
+	{
+	putchar(x);
+	x--;
+	}
+	putchar('\n');
+	return (0);
+}

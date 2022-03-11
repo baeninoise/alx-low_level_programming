f19c7726fce7e44f4d004451f7a0977d951e06f7
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-11 20:55:02 +0100

    Create 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ **/
+int main(void
+{
+int x = 0;
+
+	while (x <= 10)
+	{
+	putchar(x);
+	x++;
+	}
+	putchar('\n');
+	return (0);
+}

427a05d2b33a5f7210531b0190928e8d945c9a9d
baeninoise <amandalaleye@gmail.com>
2022-03-11 08:29:13 -0800

    erty

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	int x = 'a';
+
+	while (x <= 'z')
+	{
+	putchar(x);
+	x++;
+	}
+	return (0);
+}

7e22c9869752bd7d76cc2eca322d8ae1a1aa0697
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-15 18:16:53 +0100

    Create 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabet character
+ * @c: the parameter that contains letters in lowercase and uppercase
+ *
+ * Return: 1 if the letter is lowercase or uppercase, 0 otherwise
+ */
+int _isalpha(int c);
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

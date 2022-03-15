e9e9de6ad40f1875e1047d791fb0802d8543add4
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-15 15:01:35 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,13 +5,13 @@
  *
  * Return: Always 0 (Success)
  */
- _putchar('_');
- _putchar('p');
- _putchar('u');
- _putchar('t');
- _putchar('c');
- _putchar('h');
- _putchar('a');
- _putchar('r');
- _putchar('\n');
- return(0);
+int main(void)
+{
+ char txt[] = "_putchar\n";
+ init i
+  for(i = 0; i < 9; i++)
+ {
+ _putchar(txt[8]);
+ }
+ return (0);                                                                                               
+}

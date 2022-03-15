47140f4ea74c114161a864325d652891b85b6f53
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-15 18:43:19 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,9 +1,9 @@
 /*
  * File: 100-times_table.c
- * Auth: Brennan D Baraban
+ * Desc
  */
 
-#include "holberton.h"
+#include "main.h"
 
 /**
  * print_times_table - Prints the times table of the input,

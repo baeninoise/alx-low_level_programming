d9d2e30b55e1a0e89354e2f2ef54682bac5b94eb
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-15 17:37:47 +0100

    Create 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void);
+{
+	int x;
+	int i;
+
+	x = 'a';
+	for (i <=10)
+  {
+		while (x <= 'z')
+		{
+		_putchar(x);
+		x++;
+		}
+	_putchar('\n');
+	i++;
+	}
+}

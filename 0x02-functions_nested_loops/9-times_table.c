8ffcc8075c7be6b15e74163c039a851590e1f696
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-15 18:31:32 +0100

    Create 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: 9 times table
+ */
+void times_table(void)
+{
+	int a, b, mult;
+
+	for (a = 0 ; a <= 9 ; a++)
+	{
+		for (b = 0 ; b <= 9 ; b++)
+		{
+			mult = a * b;
+
+			if (mult <= 9)
+			{
+				if (b != 0)
+				{
+					_putchar(' ');
+				}
+				_putchar(mult + '0');
+			}
+			else
+			{
+				_putchar(mult / 10 + '0');
+				_putchar(mult % 10 + '0');
+			}
+			if (b != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}

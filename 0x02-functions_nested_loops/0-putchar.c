8b2a3957db7ff4b1926add532d85abcbfd978134
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-15 11:39:26 +0100

    Update 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ **/
+int main(void)
+{
+	string text = "_putchar";
+	int _putchar(char c);
+	
+	size_t index = 0;
+	for (char c : text) 
+	{
+	_putchar(char c)
+	} 
+	return (0);
+}

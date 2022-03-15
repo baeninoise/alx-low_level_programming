c503c83429b03e41f83ad50c151f114cb1fe9c12
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-15 18:21:49 +0100

    Create 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n: The character to print
+ *
+ * Return: 1, 0, -1.
+ */
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	if (n >= 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n <= 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	return (0);
+}

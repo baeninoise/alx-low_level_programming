cea75a340f7517692db72661f45e4482eda8d260
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-16 09:39:25 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -6,15 +6,9 @@
  * * Return: 0
  * */
 
-<<<<<<< HEAD
-void positive_or_negative()
-	
-=======
 void positive_or_negative(int i)
->>>>>>> f1bd1a0a8fff736a989f491b36bf936c794e562d
 {
-	int i;
-
 	i = 98;
 	positive_or_negative(i);
+	return (0)
 }

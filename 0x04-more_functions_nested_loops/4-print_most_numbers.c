e5df32908295e7df1c6ba54fda777f1ac82eca9b
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-17 09:28:16 +0100

    Create 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9, followed by a new line
+ * @i: the character to print
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void);
+{
+	int i
+	
+	for(i = 48; i < 58; i++)
+	{
+		if !(i == 50 && i == 52)
+		{
+		_putchar(i);
+		}
+	}
+	_putchar('\n');
+}

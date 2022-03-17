a9d723b84df261a3c94ab0fbe4d15d0985b66398
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-17 08:59:26 +0100

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit
+ * @c: the parameter to print
+ *
+ * Return: 1 or 0
+ */
+
+int _isdigit(int c);
+{
+	if (c >= 48 && c < 58)
+	{
+	return (1);
+	}
+  else
+	{
+	return (0);
+	}
+}

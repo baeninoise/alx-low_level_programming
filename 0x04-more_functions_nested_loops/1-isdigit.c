d1d4606e1274393aaefe6560d0c9bbb36d603fbe
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-17 09:03:47 +0100

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,22 +1,18 @@
-#include <stdio.h>
 #include "main.h"
 /**
- * main - Entry point
- *  
- * Return: Always 0 (Success)
- **/
-
+ * _isupper - checks for uppercase character
+ * @c: the character to print
+ *
+ * Return: 1 or 0
+ */
 int _isupper(int c);
 {
-	char c;
-	
-	c = getchar()
-		if (c == _isupper(c))
-		{
-		return (1)
-		}
-		else
-		{
-		return (0);
-		}
+	if (c >= 65 && c <=90)
+	{
+	return (1)
+	}
+	else
+	{
+	return (0);
+	}
 }

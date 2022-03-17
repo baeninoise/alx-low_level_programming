95b0df489305bef4e774c1b7b2c5f99fdf161a9c
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-17 08:42:10 +0100

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - Entry point
  *  
  * Return: Always 0 (Success)
  **/
+
+int main(void)
+{
+	char c;
+	
+	c = getchar()
+		if (c == _isupper(c))
+		{
+		return (1)
+		}
+    else
+		{
+			return (0);
+		}
+}

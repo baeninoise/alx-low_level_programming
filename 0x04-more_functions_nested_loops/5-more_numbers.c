14fad0efcc5e67cb27e3fb9828cf6ff178204638
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-17 12:45:07 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,20 +1,18 @@
 #include "main.h"
-
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
- 
- void more_numbers(void);
+void more_numbers(void)
 {
-	int x
-  int i
+	int x;
+	int i;
 	
 	x = 0;
 	while(x < 10)
 	{
-		for(i = 48; i < 58; i++)
+		for (i = 48; i < 58; i++)
 		{
 		_putchar(i);
 		}

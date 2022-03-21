2ecc0ab634f21c14a6a321296fc24d517a213b19
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-21 14:01:01 +0100

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * reset_to_98 - takes a pointer to an int as parameter and updates
- * @a,b: menber
+ * swap_int - swaps the values of two integers
+ * @a: integer
+ * @b: integer
  *
  * Return: Always 0
  */

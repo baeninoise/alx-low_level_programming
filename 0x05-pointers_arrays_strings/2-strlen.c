a8e7be38dbf8ceb2e71363c436a6c5f1e7a18bea
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-21 14:49:47 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,6 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
 /**
- * reset_to_98 - returns the length of a string
+ * strlen - returns the length of a string
  * @s: menber
  *
  * Return: Always 0

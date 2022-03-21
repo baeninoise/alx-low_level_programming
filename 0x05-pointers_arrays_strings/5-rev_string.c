3711a92a37abe94db8b36646606166b5f0127de1
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-21 15:45:24 +0100

    Create 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string -  prints a string, followed by a new line
+ * @s: menber
+ *
+ * Return: Always 0
+ */
+void rev_string(char *s)
+	{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	while (i > 0)
+	{
+		i--;
+		_putchar(s[i]);
+	}
+}

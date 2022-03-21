970544338528bd18f03fd5404b5a02b6e93b1c4d
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-21 17:59:06 +0100

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,12 +11,6 @@ void print_array(int *a, int n)
 
 	for (i = 0 ; i < n ; i++)
 	{
-		printf("%d", a[i]);
-
-		if (i < n - 1)
-		{
-			printf(", ");
-		}
+		printf("%d%c", a[i], (i != n-1) ? ',' : '\n');
 	}
-	_putchar('\n');
 }

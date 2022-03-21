288a3fb0e49f50b091606e8b17c71cd29e795a4f
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-21 13:26:36 +0100

    Create 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - takes a pointer to an int as parameter and updates
+ * @a,b: menber
+ *
+ * Return: Always 0
+ */
+
+void swap_int(int *a, int *b);
+{
+	*a = &b
+	*b = &a
+}

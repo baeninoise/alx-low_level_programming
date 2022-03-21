63c99fcf35ba0fa197711f6445cbf7229d595d3b
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-21 17:05:23 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,11 +6,11 @@
  */
 void rev_string(char *s)
 {
-int i;
+	int i;
 
-	for (i = 0 ; str[i] != '\0' ; i++)
+	for (i = 0 ; s[i] != '\0' ; i++)
 	{
-	_putchar(str[i]);
+	_putchar(s[i]);
 	}
 	while (s[i] != 0)
 	{

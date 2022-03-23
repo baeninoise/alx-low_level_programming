b8a93b9769203d6a1a443a273cb30664028ae2ae
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-23 18:06:21 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,18 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * 2-strncpy.c - compares two strings
- * @s1: menber
- * @s2: menber
+ * _strcmp - compares two strings
+ * @s1: input parameter
+ * @s2: input parameter
  *
- * Return: Always 0
+ * Return: 0 if equal or b if is different
  */
 int _strcmp(char *s1, char *s2)
 {
 	int a = 0;
 	int b = 0;
 
-	while (s1[a] != '\0')
+	while (s1[a] != '\0' && b == 0)
 	{
 		b = s1[a] - s2[a];
 		a++;

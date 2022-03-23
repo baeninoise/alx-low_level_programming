583ead2f2d14c5b12046c5df7835cb0abc46fe30
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-23 18:11:38 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,20 +1,31 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * 0-strcat.c - concatenates two strings
+ * _strncat - concatenates two strings
+ * @dest: input parameter string
+ * @src: input parameter string
+ * @n: times to iterations
  *
- * Return: Always 0
+ * Return: dest
  */
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int a;
+	int b;
 
-	for (i = 0; dest[i]!='\0'; i++)
+	a = 0;
+
+	while (dest[a] != 0)
 	{
-		for (j = 0; src[j]!='\0'; j++; i++)
-		{
-		dest[i] = src[j];
-		}
+		a++;
 	}
+
+	b = 0;
+
+	while (src[b] != 0 && b < n)
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	return (dest);
 }
-return(dest);

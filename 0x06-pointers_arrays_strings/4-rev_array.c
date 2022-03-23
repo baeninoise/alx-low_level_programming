e8984cac12a01296c29e90ad37edc5212c080875
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-23 16:03:52 +0100

    Create 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * 2-strncpy.c - copies a string
+ * @n: menber
+ *
+ * Return: Always 0
+ */
+void reverse_array(int *a, int n);
+{
+	int i;
+	
+	i = 0;
+	while(i < n)
+	{
+	i++;
+	}
+	while(i < n && i >= 0)
+	{
+	printf("%d", a[i]);
+		if (index == n - 1)
+		{
+		continue;
+		}
+	printf(", ");
+	i--;
+	}
+}

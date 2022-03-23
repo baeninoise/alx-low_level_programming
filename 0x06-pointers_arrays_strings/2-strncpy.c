a90ff5cfacfffe377a9c2f3fbd6d125beb3f73e5
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-23 13:37:57 +0100

    Create 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * 2-strncpy.c - copies a string
+ * @n: menber
+ *
+ * Return: Always 0
+ */
+char *_strncpy(char *dest, char *src, int n);
+int i, j;
+
+	i = 0;
+	for(j = 0; src[j] != '\0'; j++)
+		if(j < n)
+		{
+		dest[i] = src[j];
+		}
+	return(dest);
+	}

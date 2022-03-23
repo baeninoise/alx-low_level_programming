7c4e6a475ccf5117eb475478fa830304cf807fe9
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-23 11:58:09 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,6 +12,6 @@ char *_strcat(char *dest, char *src);
 	for (i = 0; dest[i]!='\0'; i++);
 		for (j = 0; src[j]!='\0'; j++, i++)
 		{
-		dest[i] = src[j];
+		src[i] = src[j];
 		}
-	scr[i] = '\0';
+	src[i] = '\0';

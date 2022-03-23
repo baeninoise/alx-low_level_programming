8e5fa2a93d051a5c2bac0f9a5ba7275a8713efb2
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-23 12:12:19 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,3 +6,17 @@
  *
  * Return: Always 0
  */
+char *_strncat(char *dest, char *src, int n)
+
+int i, j;
+
+	while(dest[i] != '\0')
+		i++;
+	if(i >= n)
+	{
+		for(j = 0; dest[j] != '\0'; i++)
+		{
+		dest[j] = src[i];
+		}
+	dest[j] = '\0';
+	}

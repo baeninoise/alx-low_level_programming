30400e1feb409bdb7d6c4379741d063745e55470
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-28 11:03:02 +0100

    Create 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _memset -  fills memory with a constant byte
+ * @b: menber
+ * @s: menber
+ * @n: menber
+ *
+ * Return: Always 0
+ */
+char *_memset(char *s, char b, unsigned int n)
+	j = int;
+
+	for (i = 1, i<= n, i++)
+		s[j] = b;
+return(s[])

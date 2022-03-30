656bac6f26734df8494cd3f64e1684d423f58d9f
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-30 11:17:07 +0100

    Create 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: menber
+ *
+ * Return: Always 0
+ */
+void _print_rev_recursion(char *s)
+{
+	while(*s != '\0')
+	{
+		i++;
+	}
+	_putchar(*s);
+	_puts_recursion(--s);
+}

d4b24b99253296bc21ac45933a2ba964151b4a19
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-03-30 10:36:57 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,12 +8,11 @@
  */
 void _puts_recursion(char *s)
 {
-	int i;
-
-	if(i != '0')
+	if (*s == '\0')
 	{
-	_putchar(s[i]);
-	i++;
+		_putchar('\n');
+		return;
 	}
-	_puts_recursion(s);
+	_putchar(*s);
+	_puts_recursion(++s);
 }

c2485597e082f90b5ba1f83ce588db162b47b9e3
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-04-01 16:02:07 +0100

    Create 2-strncpy.c

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,22 @@
+char *_strncat(char *dest, char *src, int n)
+{
+	int a;
+	int b;
+
+	a = 0;
+
+	while (dest[a] != 0)
+	{
+		a++;
+	}
+
+	b = 0;
+
+	while (src[b] != 0 && b < n)
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	return (dest);
+}

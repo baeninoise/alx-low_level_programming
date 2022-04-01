94a2eebd643def2706f7f3bed37ee4932932bb11
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-04-01 15:24:04 +0100

    Update _putchar.c

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,19 +1,13 @@
-#include "main.h"
 #include <unistd.h>
-#include <stdio.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
-}
-
-int _islower(int c)
-{
-	if (c >= 97 && c <= 122)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
 }

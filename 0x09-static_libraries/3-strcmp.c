04837b4a8d2c72752cad14d8962a12239a86a720
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-04-01 14:27:06 +0100

    Create 3-strcmp.c

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,27 @@
+int _strcmp(char *s1, char *s2)
+{
+	int a = 0;
+	int b = 0;
+
+	while (s1[a] != '\0' && b == 0)
+	{
+		b = s1[a] - s2[a];
+		a++;
+	}
+	return (b);
+}
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+
+
+
+}

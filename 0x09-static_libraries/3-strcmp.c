d4af78eeee111869cca44ce07e87cdc723b1cee6
baeninoise <amandalaleye@gmail.com>
2022-04-01 08:35:12 -0700

    yut

--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -10,18 +10,3 @@ int _strcmp(char *s1, char *s2)
 	}
 	return (b);
 }
-
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int i = 0;
-
-	while (i < n)
-	{
-		s[i] = b;
-		i++;
-	}
-	return (s);
-
-
-
-}

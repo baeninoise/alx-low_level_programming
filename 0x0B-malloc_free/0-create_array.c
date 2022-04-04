c82695baabf9202092d63ad2c7c9d62b351c8bf7
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-04-04 16:37:01 +0100

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,7 +8,7 @@
  *
  * Return: pointer to the array (Success), NULL (Error)
  */
-char *create_array(unsigned int size, char c);
+char *create_array(unsigned int size, char c)
 {
 	char *p;
 	unsigned int i = 0;

5f92b4674e6dff2816116400df5838cc95adc065
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-04-07 11:54:18 +0100

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,11 +10,11 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *mem;
+	char *mem;
 	unsigned int i;
 	
 	if(nmemb == 0 || size == 0)
-		returns (NULL);
+		return (NULL);
 
 	mem = malloc(nmemb * size);
 

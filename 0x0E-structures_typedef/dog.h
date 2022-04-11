0092e5f9fb9f9a8f2bb3c8c9e40168d1bd7d9aff
baeninoise <99394368+baeninoise@users.noreply.github.com>
2022-04-11 11:29:24 +0100

    Update dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - Short description
+ * @name: first member
+ * @age: second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	name, type = char *;
+	age, type = float;
+	owner, type = char *;
+};
